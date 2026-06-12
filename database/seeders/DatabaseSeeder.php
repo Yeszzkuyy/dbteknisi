3897af10d8f35c8f9ff8cf77d9b12d58ea01aea0
Yeski <yehezkielmayogi.ptnti@gmail.com>
2026-06-12 13:51:42 +0700

    Add master tables and work type seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,26 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\WorkType;
 
-class DatabaseSeeder extends Seeder
+class WorkTypeSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $types = [
+            'Demo Produk',
+            'Survey',
+            'POC',
+            'Instalasi',
+            'Troubleshooting',
+            'Maintenance',
+        ];
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        foreach ($types as $type) {
+            WorkType::create([
+                'name' => $type
+            ]);
+        }
     }
 }
