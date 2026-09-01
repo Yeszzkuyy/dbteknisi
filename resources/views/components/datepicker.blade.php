c594d220d5625a978a6df6c0866c4331460930b1
Yeski <yehezkielmayogileons@gmail.com>
2026-09-01 10:48:12 +0700

    refactor(leads): tabel SaaS CRM - checkbox batch, ghost action buttons, badge cabang, filter compact dark mode

--- resources/views/components/datepicker.blade.php
+++ resources/views/components/datepicker.blade.php
@@ -1,4 +1,4 @@
 @props(['disabled' => false])
 
-<input type="date" {{ $attributes->merge(['class' => 'w-full rounded-xl border-slate-300 focus:border-blue-500 focus:ring-blue-500 cursor-pointer']) }} @disabled($disabled)
+<input type="date" {{ $attributes->merge(['class' => 'w-full rounded-xl border-slate-300 bg-white text-slate-800 dark:bg-slate-900 dark:border-slate-700 dark:text-slate-200 focus:border-blue-500 focus:ring-blue-500 dark:focus:border-blue-400 cursor-pointer']) }} @disabled($disabled)
        onfocus="this.showPicker()">
